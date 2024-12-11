4a45dde7f80666f47662194e44e0050e6278fa93
johnwaugh1 <waughjm@appstate.edu>
2024-12-11 14:28:03 -0500

    Started about section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,16 @@
 const About = () => {
   return (
-    <div>About</div>
+    <section id="about" className="w-full items-start gap-10 max-container">
+      <h1 className="text-4xl text-center font-coda">Hi, I'm John Waugh</h1>
+      <br />
+      <p className="text-2xl text-center font-coda leading-8">
+        A Computer Science student at Appalachian State University, currently working as
+        an Embedded Systems Engineer and Race Operations Lead at Team Sunergy, where I help design, build, and race solar-powered cars. My responsibilities include enhancing telemetry systems, managing team operations, and handling fundraising and sponsorship efforts.
+        In addition, I serve as the Composites Specialist, designing and fabricating lightweight, structurally strong components using advanced composite materials like fiberglass and carbon fiber.
+        I've completed coursework in Data Structures, Software Engineering, Computer Systems, and Data Analysis, and have strengthened my skills with online courses in AI/ML and web development.
+        I'm actively seeking an internship in AI/ML, web development, embedded systems, or full-stack development, where I can contribute to impactful projects and further develop my expertise.
+      </p>
+    </section>
   )
 }
 
